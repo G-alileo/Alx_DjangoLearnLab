d9f5b2bc90c4c7215a08610495522f5cdd7b14c8
G_alileo <jamespmurithi@gmail.com>
2025-02-23 21:27:30 +0300

    Implementation of role based access control system

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,8 +7,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
+# from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 
@@ -41,32 +40,32 @@ def register(request):
     return render(request, "relationship_app/register.html", {"form": form})
 
 
-def is_admin(user):
-    return user.is_authenticated and user.userprofile.role == 'ADMIN'
+# def is_admin(user):
+#     return user.is_authenticated and user.userprofile.role == 'ADMIN'
 
-def is_librarian(user):
-    return user.is_authenticated and user.userprofile.role == 'LIBRARIAN'
+# def is_librarian(user):
+#     return user.is_authenticated and user.userprofile.role == 'LIBRARIAN'
 
-def is_member(user):
-    return user.is_authenticated and user.userprofile.role == 'MEMBER'
+# def is_member(user):
+#     return user.is_authenticated and user.userprofile.role == 'MEMBER'
 
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'admin_dashboard.html', {
-        'title': 'Admin Dashboard',
-        'content': 'Welcome to the Admin Dashboard'
-    })
+# @user_passes_test(is_admin)
+# def admin_view(request):
+#     return render(request, 'admin_dashboard.html', {
+#         'title': 'Admin Dashboard',
+#         'content': 'Welcome to the Admin Dashboard'
+#     })
 
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'librarian_dashboard.html', {
-        'title': 'Librarian Dashboard',
-        'content': 'Welcome to the Librarian Dashboard'
-    })
+# @user_passes_test(is_librarian)
+# def librarian_view(request):
+#     return render(request, 'librarian_dashboard.html', {
+#         'title': 'Librarian Dashboard',
+#         'content': 'Welcome to the Librarian Dashboard'
+#     })
 
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'member_dashboard.html', {
-        'title': 'Member Dashboard',
-        'content': 'Welcome to the Member Dashboard'
-    })
+# @user_passes_test(is_member)
+# def member_view(request):
+#     return render(request, 'member_dashboard.html', {
+#         'title': 'Member Dashboard',
+#         'content': 'Welcome to the Member Dashboard'
+#     })
