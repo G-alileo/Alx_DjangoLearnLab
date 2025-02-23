eb7ae9bcb45f41d100be3ebfe89600085ed2e363
G_alileo <jamespmurithi@gmail.com>
2025-02-23 21:08:03 +0300

    Implementation of role based access control system

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,8 @@ from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
@@ -24,6 +26,20 @@ class UserLoginView(LoginView):
 
 class UserLogoutView(LogoutView):
     template_name = "relationship_app/logout.html"
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  
+            return redirect("home") 
+    else:
+        form = UserCreationForm()
+    
+    return render(request, "relationship_app/register.html", {"form": form})
+
 
 def is_admin(user):
     return user.is_authenticated and user.userprofile.role == 'ADMIN'
