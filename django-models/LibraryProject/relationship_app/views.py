f5068bc7829a866c12f4a16f9a1bc4e380a85810
G_alileo <jamespmurithi@gmail.com>
2025-02-23 19:34:28 +0300

    Implementing user authentification

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Book
 from .models import Library
@@ -6,8 +6,8 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic import CreateView
 from django.contrib.auth import login
+
 # Create your views here.
 
 def list_books(request):
@@ -25,7 +25,15 @@ class UserLoginView(LoginView):
 class UserLogoutView(LogoutView):
     template_name = "relationship_app/logout.html"
 
-class UserRegisterView(CreateView):
-    template_name = "relationship_app/register.html"
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  
+            return redirect("home") 
+    else:
+        form = UserCreationForm()
+    
+    return render(request, "relationship_app/register.html", {"form": form})
