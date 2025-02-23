1e248b1b4d832a2799d4f459d24170c3ef3c20fd
G_alileo <jamespmurithi@gmail.com>
2025-02-23 18:33:12 +0300

    Addition of relationship_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book, Library
+from django.views.generic import DetailView
+
 
 # Create your views here.
+
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    book_list = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(f"<pre>{book_list}</pre>")
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = "library"
