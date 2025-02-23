7ead0d824069ec7e5b3d4a124c551b04772c9807
G_alileo <jamespmurithi@gmail.com>
2025-02-23 22:28:07 +0300

    Implementation of role based access control system

--- django-models/LibraryProject/relationship_app/decorator.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.decorators import user_passes_test
-from django.core.exceptions import PermissionDenied
-
-def role_required(role):
-    def check_role(user):
-        if not user.is_authenticated:
-            return False
-        return user.userprofile.role == role
-    return user_passes_test(check_role, login_url='login')
