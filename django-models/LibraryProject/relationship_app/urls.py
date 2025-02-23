c710a91a2012a2d95624cfbf483398eb338340dd
G_alileo <jamespmurithi@gmail.com>
2025-02-23 21:51:27 +0300

    Implementation of role based access control system

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from relationship_app.views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
-from .views.admin_view import admin_view
-from .views.librarian_view import librarian_view
-from .views.member_view import member_view
-
+from relationship_app.view.admin_view import admin_dashboard
+from relationship_app.view.librarian_view import librarian_dashboard
+from relationship_app.view.member_view import member_dashboard
 
 urlpatterns = [
     path("books/", list_books, name="list_books"), 
@@ -13,7 +12,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('admin-dashboard/', admin_view, name='admin_dashboard'),
-    path('librarian-dashboard/', librarian_view, name='librarian_dashboard'),
-    path('member-dashboard/', member_view, name='member_dashboard'),
+    path("admin-dashboard/", admin_dashboard, name="admin_dashboard"),
+    path("librarian-dashboard/", librarian_dashboard, name="librarian_dashboard"),
+    path("member-dashboard/", member_dashboard, name="member_dashboard"),
 ]
