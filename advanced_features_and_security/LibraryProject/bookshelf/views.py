777e32d974b6b2b6a5ff8ed928ed5726980cb82d
G_alileo <jamespmurithi@gmail.com>
2025-03-03 18:12:58 +0300

    Added documentation to my code

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import permission_required
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import ensure_csrf_cookie
 from .models import Book
-from .forms import BookForm, ExampleForm
+from .forms import ExampleForm, BookForm
 
 
 # Create your views here.
