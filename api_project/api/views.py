199679caaa63d938420b93f504d38063e9011606
G_alileo <jamespmurithi@gmail.com>
2025-03-09 22:57:16 +0300

    Adding documentation for my permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,16 +2,16 @@ from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]  #Allow any user to access
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAuthenticated]  # Only authenticated users can access
