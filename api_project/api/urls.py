dfb747e10067695514296ba223af7e36d00d7614
G_alileo <jamespmurithi@gmail.com>
2025-03-09 22:34:23 +0300

    created a BookViewSet class

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import BookList
+from rest_framework.routers import DefaultRouter
+
+
+
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
