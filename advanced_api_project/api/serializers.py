d30dfb04822a8c37f903dd9fedc466f5bfb94417
G_alileo <jamespmurithi@gmail.com>
2025-03-16 01:49:23 +0300

    Addition of a custom validation method to ensure that publication year is not greater than current year plus documentation to my code

--- advanced_api_project/api/serializers.py
+++ advanced_api_project/api/serializers.py
@@ -2,13 +2,21 @@ from rest_framework import serializers
 from .models import Author, Book
 from datetime import date
 
+
+# Serializes all book fields.
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
 
+    # Ensuring that the publication year is not greater than current date
+    def validate_publication_year(self, value):
+        current_year = date.today().year
+        if value > current_year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
 
-
+# Serializes the author's name and uses nested serialization to include related books.
 class AuthorSerializer(serializers.ModelSerializer):
     books = BookSerializer(many=True, read_only=True)  # This is my nested serializer
 
