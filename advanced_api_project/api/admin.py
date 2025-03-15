2f7e7c4c3a19cb65e7b5ce945106991193d88ae2
G_alileo <jamespmurithi@gmail.com>
2025-03-16 02:39:00 +0300

    Fixed my file structure, modified my admin file to include filter options on the admin panel

--- advanced_api_project/api/admin.py
+++ advanced_api_project/api/admin.py
@@ -3,5 +3,16 @@ from .models import Author, Book
 
 # Register your models here.
 
-admin.site.register(Author)
-admin.site.register(Book)
+# admin.site.register(Author)
+# admin.site.register(Book)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'publication_year', 'author')
+    list_filter = ('publication_year', 'author')
+    search_fields = ('title',)
