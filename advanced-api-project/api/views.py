3f305c440071976418195c5d27e44ba9419ad0f5
G_alileo <jamespmurithi@gmail.com>
2025-03-16 13:14:33 +0300

    creation of generic views on views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+
+
+# ListView - Retrieve all books (Read-Only)
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+# DetailView - Retrieve a single book by ID (Read-Only)
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+# CreateView - Add a new book (Restricted to Authenticated Users)
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# UpdateView - Modify an existing book (Restricted to Authenticated Users)
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# DeleteView - Remove a book (Restricted to Admin Users Only)
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAdminUser]
