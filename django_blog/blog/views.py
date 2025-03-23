9fcd678150a11b78bb23c1a624d7afd7b9b0f020
G_alileo <jamespmurithi@gmail.com>
2025-03-23 11:11:37 +0300

    Debugging errors

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from .forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def home(request):
     return render(request, 'blog/home.html')
+
+def posts(request):
+    return render(request, 'blog/posts.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Welcome back, {username}!')
+                return redirect('home')  # Redirect to home page after login
+            else:
+                messages.error(request, 'Invalid username or password.')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'blog/login.html', {'form': form})
 
 def register(request):
     if request.method == 'POST':
