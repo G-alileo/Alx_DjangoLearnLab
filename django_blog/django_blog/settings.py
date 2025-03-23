befa9f0d44e239631e4da640a190285f7cf19f05
G_alileo <jamespmurithi@gmail.com>
2025-03-23 09:40:24 +0300

    Configuring django to find my static files and migrations

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,7 +120,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # configuring django to find my static files
-import os
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'blog/static')
 ]
