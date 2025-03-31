fc10c3223ecdd8f3ea77f1b75d2072c8a6958ec9
G-alileo <jamespmurithi@gmail.com>
2025-03-31 17:23:23 +0300

    Modified UserSerializer

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,14 +1,23 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
+from .models import CustomUser
 
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
 
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email','bio', 'profile_picture', 'followers_count', 'following_count']
+
+    def get_followers_count(self, obj):
+        return obj.followers.count()
+
+    def get_following_count(self, obj):
+        return obj.following.count()
 
 class RegisterSerializer(serializers.Serializer):
     username = serializers.CharField()
@@ -23,3 +32,4 @@ class RegisterSerializer(serializers.Serializer):
         )
         Token.objects.create(user=user)
         return user
+    
