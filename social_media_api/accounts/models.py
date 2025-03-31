c82828df566539a6ce6af67922dddeff4e1d9bbf
G-alileo <jamespmurithi@gmail.com>
2025-03-31 17:17:03 +0300

    Addition of a following field to CustomUser

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
-    followers = models.ManyToManyField("self", symmetrical=False, related_name="following", blank=True)
+    followers = models.ManyToManyField("self", symmetrical=False, related_name="followings", blank=True)
+    following = models.ManyToManyField("self", symmetrical=False, related_name="follower", blank=True)
 
     def __str__(self):
         return self.username
