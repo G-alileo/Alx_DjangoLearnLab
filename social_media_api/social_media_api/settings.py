7c2c5c1d7846e0211c36678a3742245d4c1980c3
G-alileo <jamespmurithi@gmail.com>
2025-04-01 19:09:53 +0300

    collecting static file

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,3 +151,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+# STATIC FILES CONFIGURATION
+STATIC_URL = '/static/'  # URL for serving static files
+
+# Collect all static files into this directory when running `collectstatic`
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
