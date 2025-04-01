63c379ee01cc5ddbbf463ba8fb7deb787667c0d9
G-alileo <jamespmurithi@gmail.com>
2025-04-01 08:57:41 +0300

    addition of like(s) model

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=255)
@@ -21,3 +25,14 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author.username} on {self.post.title}"
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')  # Prevents duplicate likes
+
+    def __str__(self):
+        return f"{self.user} likes {self.post}"
