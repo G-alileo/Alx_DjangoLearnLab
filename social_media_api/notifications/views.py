66833e3b404349b29008a4eb54d57c4cb26a12b0
G-alileo <jamespmurithi@gmail.com>
2025-04-01 09:06:39 +0300

    Addition of a notification system

--- social_media_api/notifications/views.py
+++ social_media_api/notifications/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from .models import Notification
+from .serializers import NotificationSerializer
 
 # Create your views here.
+
+@api_view(['GET'])
+@permission_classes([permissions.IsAuthenticated])
+def user_notifications(request):
+    notifications = Notification.objects.filter(recipient=request.user).order_by('-timestamp')
+    serializer = NotificationSerializer(notifications, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
